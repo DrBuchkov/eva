acff57510fd67614f725a8ed247c55744fcf1b31
Nikolas Pafitis <silencioseu@gmail.com>
2021-10-28 17:04:59 +0300

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to eva
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/eva/core_test.clj
@@ -0,0 +1,7 @@
+(ns eva.core-test
+  (:require [clojure.test :refer :all]
+            [eva.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
